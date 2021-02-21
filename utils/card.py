86b1439a9ae041be0dfdd185e554d94d7953b6ef
kamisch <chengtwv2@gmail.com>
2021-02-21 14:41:06 -0500

    added base64 image hashing

--- utils/card.py
+++ utils/card.py
@@ -1,9 +1,14 @@
+from utils.response import imgUrlToBase64
+
+
 def renderUserCard(playerName: str, playerUrl: str, playerAvatarUrl: str):
+    base64Strhash = imgUrlToBase64(playerAvatarUrl)
+    dataImage = 'data:image/jpeg;charset=utf-8;base64,' + base64Strhash
     body =  """<svg xmlns="http://www.w3.org/2000/svg" width="150" height ="160" version="1.1">
           <rect x="0" y="0" width="120" height="140" stroke="black" fill="transparent" stroke-width="2"/>
           <image href="{}" x="10" y="10" height="100" width="100"/>
           <text x ='60' y ='130' text-anchor="middle">{}</text>
-          </svg>""".format(playerAvatarUrl,playerName)
+          </svg>""".format(dataImage,playerName)
 
     return body
 
--- utils/response.py
+++ utils/response.py
@@ -1,3 +1,8 @@
+import base64
+import requests
+
+def imgUrlToBase64(url):
+    return base64.b64encode(requests.get(url).content).decode('utf-8')
 
 def sortOwnGames(data):
     res = sorted(data['games'], key=lambda x : x['playtime_forever'], reverse=True)
