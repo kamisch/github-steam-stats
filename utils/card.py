6ac36459d7238d19aac95923b3caefabc5423eb9
kamisch <chengtwv2@gmail.com>
2021-02-21 20:28:38 -0500

    fixed game cards layout

--- utils/card.py
+++ utils/card.py
@@ -5,10 +5,10 @@ def renderUserCard(playerName: str, playerUrl: str, playerAvatarUrl: str,width:i
     dataImage = imgUrlToBase64DataImage(playerAvatarUrl)
     
     body =  """<svg xmlns="http://www.w3.org/2000/svg" width="150" height ="160" version="1.1">
-          <rect x="0" y="0" rx="4.5" width="120" height="140" stroke="black" fill='{}' stroke-width="2"/>
-          <image href="{}" x="10" y="10" height="100" width="100"/>
-          <text x ='60' y ='130' text-anchor="middle" fill = "{}">{}</text>
-          </svg>""".format(bgColor,dataImage,textColor,playerName)
+          <rect x="0" y="0" rx="4.5" width="120" height="140" stroke="{boarderColor}" fill='{bgColor}' stroke-width="{boarderWidth}"/>
+          <image href="{dataImage}" x="10" y="10" height="100" width="100"/>
+          <a href="{playerUrl}"><text x ='60' y ='130' text-anchor="middle" font-family="Consolas, monospace" textfill = "{textColor}">{playerName}</text></a>
+          </svg>""".format(playerName=playerName, playerUrl=playerUrl, dataImage=dataImage, bgColor=bgColor,textColor=textColor, boarderColor=boarderColor,boarderWidth=boarderWidth)
 
     return body
 
@@ -16,13 +16,13 @@ def renderGame(gameName:str, gameId:int, gameTime:int, gameHash:int,x:int, y:int
     gameImgUrl = "http://media.steampowered.com/steamcommunity/public/images/apps/{}/{}.jpg".format(gameId,gameHash)
     dataImage = imgUrlToBase64DataImage(gameImgUrl)
     gameHours = round(gameTime/60,1)
-    return """<svg xmlns="http://www.w3.org/2000/svg" x ="{x}" y="{y}" width="{width}" height ="{height}" version="1.1">
+    body = """<svg xmlns="http://www.w3.org/2000/svg" x ="{x}" y="{y}" width="{width}" height ="{height}" version="1.1">
           <title>{gameName}</title>
-          <rect  width="{width}" height="{height}" stroke="white" fill="transparent" stroke-width="2"/>
-          <image x="10" y="0" href="{dataImg}"  height="100" width="100"/>
-          <text x ='60' y ='90' text-anchor="middle" fill="black">{gameHours} hours</text>
-          </svg>""".format(width=width, height=height, x=x, y=y, gameHours=gameHours, dataImg= dataImage,gameName=gameName)
-
+          <rect  width="{width}" height="{height}" stroke="{boarderColor}" fill="{bgColor}" stroke-width="{boarderWidth}"/>
+          <image x="10" y="0" href="{dataImg}"  height="50" width="100"/>
+          <text x ='60' y ='60' text-anchor="middle" font-family="Consolas, monospace" fill="{textColor}">{gameHours} hours</text>
+          </svg>""".format(width=width, height=height, x=x, y=y, gameHours=gameHours, dataImg= dataImage,gameName=gameName,bgColor=bgColor,textColor=textColor, boarderColor=boarderColor,boarderWidth=boarderWidth)
+    return body
 
 def renderOwnedGamesCard(data, row:int, column:int,width:int, height:int, bgColor:str, textColor:str, boarderColor:str, boarderWidth:int):
     gameCards = ""
