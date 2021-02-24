38d41a6c237880f801fac0dd2bf3f77681d9880c
kamisch <chengtwv2@gmail.com>
2021-02-23 20:40:42 -0500

    removed width and height from api parameters for now

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ async def favicon():
 
 @app.get("/api/getPlayerSummaries/{steamId}")
 async def playerSummaries(steamId: str,
-                          width: Optional[int] = 150,
-                          height:Optional[int] = 160,
                           bgColor: Optional[str] = 'ffffff',
                           textColor: Optional[str] = '000000',
                           boarderColor: Optional[str] = '000000',
                           boarderWidth: Optional[str] = '2'):
     #hex or transparent
+    width = 150
+    height= 160
     res = await getPlayerSummaries(steamId)
     card = renderUserCard(res['playerName'], res['playerProfileUrl'],
                           res['avatar'],width,height, bgColor, textColor,boarderColor,boarderWidth)
@@ -45,8 +45,6 @@ async def playerSummaries(steamId: str,
                           limit: Optional[int] = 6,
                           row: Optional[int] = 1,
                           col: Optional[int] = 12,
-                          width: Optional[int] = 120,
-                          height:Optional[int] = 70,
                           bgColor: Optional[str] = 'ffffff',
                           textColor: Optional[str] = '000000',
                           boarderColor: Optional[str] = '000000',
@@ -54,6 +52,8 @@ async def playerSummaries(steamId: str,
     if (limit < col):
         col = limit
     res = await getOwnedGames(steamId, limit)
+    width = 120
+    height= 70
     card = renderOwnedGamesCard(res, row, col,width,height,bgColor,textColor,boarderColor,boarderWidth)
     contentControlHeader = "public, max-age={}".format(os.getenv('CACHE_AGE'))
 
